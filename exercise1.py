748c817199b20bafb1c5295f3bb362d5aea65dec
Hibashahzad11 <shahzadhiba11@gmail.com>
2025-03-06 14:25:29 +0400

    Delete exercise1.py

--- exercise1.py
+++
@@ -1,8 +0,0 @@
-#Assigning value to variable 1
-word1="coding"
-#assigning value to variable 2
-word2="is"
-#assigning value to variable 3
-word3="cool"
-#use string concatentation to combine the variables and print the values
-print(word1+" "+word2+" "+word3)
