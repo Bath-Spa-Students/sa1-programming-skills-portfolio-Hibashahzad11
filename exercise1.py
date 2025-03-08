92591ee0ebb9cfc344b27c8e5c8f79d3e2c8604a
Hibashahzad11 <shahzadhiba11@gmail.com>
2025-03-08 11:59:02 +0400

    ex1.py

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+# Assigning value to variable 1 
+word1 = "coding"
+#assigning value to variable 3 
+word2 = "is"
+#assigning value to variable 3 
+word3 = "cool" 
+# corret principle statement 
+print(word1+" "+word2+" "+word3)
