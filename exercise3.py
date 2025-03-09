8abae467340fcb6dbbe7760f821dc63fcf0ed048
Hibashahzad11 <shahzadhiba11@gmail.com>
2025-03-09 04:11:11 +0400

    exercise3.py

---
+++ exercise3.py
@@ -0,0 +1,10 @@
+# this is a comment 
+name = "hiba shahzad"
+# this is a comment 
+hometown = "uae/pakistan"
+# this is a comment 
+age = "18"
+#print the result 
+print(name)
+print(hometown)
+print(age)
