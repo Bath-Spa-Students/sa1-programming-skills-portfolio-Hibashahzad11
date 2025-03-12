d8bfe2dbf0dea388d16d814cb8841858adcee1b8
Hibashahzad11 <shahzadhiba11@gmail.com>
2025-03-12 20:23:34 +0400

    exercise4.py

--- exercise4.py
+++ exercise4.py
@@ -1,6 +1,60 @@
 # asking user capital  of france
-answer=input("what is the capital of france?")
+answer=input("what is the capital of France?")
 #checking if the answer is correct
-if answer.lower()== "paris!":
+if answer.strip().lower() == "paris":  # Case insensitive check
+ print("correct!")
+else : print("wrong! the answer is Paris.")
+# asking user capital of germany 
+answer=input("what is the capital of Germany?")
+#checking if the answer is correct
+if answer.strip().lower() == "berlin":  # Case insensitive check
     print("correct!")
-else : print("wrong! the answer is paris.")
+else : print("wrong! answer is berlin.")
+# asking user capital of spain
+answer=input("what is the capital of Spain?")
+#checking if the answer is correct
+if answer.strip().lower() == "madrid": # Case insensitive check
+          print ("correct!")
+else: print("wrong! answer is Madrid.")
+# asking user capital of sweeden 
+answer=input("what is the capital of Sweden?")
+#checking if the answer is correct
+if  answer.strip().lower()== "stockholm": # Case insensitive check
+       print("correct!")
+else: print("wrong! answer is Stockholm")
+# asking user capital of russia 
+answer=input("what is the capital of Russia?")
+#checking if the answer is correct
+if answer.strip().lower()== "moscow": # Case insensitive check 
+      print("correct!")
+else: print("wrong! answer is Moscow.")
+# asking user capital of portugal
+answer=input("what is the capital of Portugal?")
+#checking if the answer is correct
+if answer.strip().lower()== "lisbon": # Case insensitive check 
+    print("correct!")
+else: print("wrong! answer is Lisbon.")
+# asking user capital of united kingdom
+answer=input("what is the capital of United Kingdom?")
+#checking if the answer is correct
+if answer.strip().lower()== "london": # Case insensitive check
+ print("correct!")
+else: print("wrong! answer is London")
+# asking user capital of poland
+answer=input("what is the capital of Poland?")
+#checking if the answer is correct
+if answer.strip().lower()== "warsaw": # Case insensitive check 
+      print("correct!")
+else: print("wrong! answer is Warsaw")
+# asking user capital of belgium 
+answer=input("what is the capital of Belgium?")
+#checking if the answer is correct
+if answer.strip().lower()== "brussels": # Case insensitve check 
+    print("correct!")
+else  : print ("wrong! answer is Brussels")
+# asking user capital of italy
+answer=input("what is the capital of Italy?")
+#checking if the answer is correct
+if answer.strip().lower()== "rome": # Case insensitive check 
+ print("correct!")
+else : print("wrong! answer is Rome")
