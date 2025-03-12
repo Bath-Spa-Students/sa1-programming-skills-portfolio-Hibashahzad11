61bfd945906f38785cbed21e12a398432f4c6836
Hibashahzad11 <shahzadhiba11@gmail.com>
2025-03-12 17:29:46 +0400

    exersice4.py

---
+++ exercise4.py
@@ -0,0 +1,6 @@
+# asking user capital  of france
+answer=input("what is the capital of france?")
+#checking if the answer is correct
+if answer.lower()== "paris!":
+    print("correct!")
+else : print("wrong! the answer is paris.")
